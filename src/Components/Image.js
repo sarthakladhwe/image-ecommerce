9290990597caaa5698f2d9d1ca1093b9b47466b0
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-21 16:58:11 +0530

    Fav and add cart icon on hover

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 function Image({img, className}) {
+
+    const [hovered, setHovered] = React.useState(false);
+
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
-        <div className={`${className} image-container`} >
+        <div 
+            className={`${className} image-container`} 
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <img src={img.url} className='image-grid' />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
