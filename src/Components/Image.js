5c85e1fff6247c06c4ddbe573040d09e4a17c569
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-21 18:34:27 +0530

    Added PropTypes to Image

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+
 import { Context } from '../Context';
 
 function Image({img, className}) {
@@ -7,7 +9,13 @@ function Image({img, className}) {
 
     const [hovered, setHovered] = React.useState(false);
 
-    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+    const heartIcon = hovered && 
+        !img.isFavorite && 
+        <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+
+    const filledHeartIcon = img.isFavorite && 
+        <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
     return (
@@ -18,9 +26,19 @@ function Image({img, className}) {
         >
             <img src={img.url} className='image-grid' alt='' />
             {heartIcon}
+            {filledHeartIcon}
             {cartIcon}
         </div>
     )
+}
+
+Image.propTypes = {
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    }),
+    className: PropTypes.string
 }
 
 export default Image
