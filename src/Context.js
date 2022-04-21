f1cc5f58944b5d233afbc3ad24fd62fc28200496
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-04-21 09:20:07 +0530

    State in context for images

--- src/Context.js
+++ src/Context.js
@@ -3,8 +3,11 @@ import React from 'react'
 const Context = React.createContext()
 
 function ContextProvider(props) {
+
+    const [images, setImages] = React.useState([])
+
     return (
-        <Context.Provider value={"sss"}>
+        <Context.Provider value={{images}}>
             {props.children}
         </Context.Provider>
     )
